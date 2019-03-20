e45cea07548ef4cf2f65d40006506b5d30cf6343
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-20 18:43:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :artists
   resources :songs
 
-  resources :artists do 
-    resources :songs 
+  resources :artists do
+    resources :songs
   end
 end
